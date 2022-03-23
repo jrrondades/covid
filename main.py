5eee2e9adf0b85aba1bab87f60994ff56637cd45
jrrondades <jrronda.des@gmail.com>
2022-03-23 14:49:23 +0000

    Leyendo y sumando datos

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ def app():
     print(sheet.title.upper())
     print("#######")
     print()
-
-    for row_cell in sheet['A1199':'K1201']:
-        for cell in row_cell:
-            print(cell.value)
+    incremento = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    sumar = ["M", "V"]
+    for row_cell in sheet['A1199':'M1213']:
+        if row_cell[0].value in sumar:
+            for i in incremento:
+                print(i)
+            incremento = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        if row_cell[2].value == "Incremento":
+            for i in range(3, 13):
+                incremento[i-3] += row_cell[i].value
+                #print(incremento[i-3])   
 
 
 if __name__ == "__main__":
